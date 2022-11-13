29d0df958cd778b162cedc70956fc9a6379b3c32
battoubattou <alicebatte@hotmail.fr>
2022-11-13 19:03:04 +0100

    adding parameters and comments in function

--- utils.py
+++ utils.py
@@ -1,15 +1,31 @@
+"""Creation of the 2 moons Dataset
+
+@duquennel
+@battoubattou
+
+13/11/2022
+"""
+
+import os
 import torch
 from torch.utils.data import Dataset
-# from torchvision import datasets
-# from torchvision.transforms import ToTensor
-# import matplotlib.pyplot as plt
 from sklearn import datasets
 import numpy as np
-import os
 
 
-def create_data(name, path, n_sample, n_point, shuffle=True, noise=None, random_state=None):
-    path_join = os.path.join(path, name)
+def create_moon_data(name_folder: str, path_folder: str, n_sample: int, n_point: int, shuffle=True, noise=None, random_state=None):
+    """
+    Create a dataset of two moons.
+
+    :param name_folder: Name of the folder to load the data in (can be created).
+    :param path_folder: Path to the folder.
+    :param n_sample: Number of samples created.
+    :param n_point: Number of points in each data sample
+    :param shuffle: Whether to shuffle the points.
+    :param noise: Standard deviation of Gaussian noise added to the data.
+    :param random_state: Determines random number generation for dataset shuffling and noise.
+    """
+    path_join = os.path.join(path_folder, name_folder)
     os.mkdir(path_join)
     for i in range(n_sample):
         arr, _ = datasets.make_moons(n_point, shuffle=shuffle, noise=noise, random_state=random_state)
