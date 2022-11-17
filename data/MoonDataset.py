1b0dbabaaa9f863f2212255d1784ce5d1ad79150
battoubattou <alicebatte@hotmail.fr>
2022-11-17 12:00:12 +0100

    show function of the data

--- data/MoonDataset.py
+++ data/MoonDataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from torch.utils.data import Dataset
 
@@ -15,6 +16,7 @@ class MoonDataset(Dataset):
         :param random_state: Determines random number generation for dataset shuffling and noise.
         """
         self.samples, _ = datasets.make_moons(n_sample, shuffle=shuffle, noise=noise, random_state=random_state)
+        self.noise = noise
 
     def __len__(self):
         """Returns the number of samples"""
@@ -23,3 +25,12 @@ class MoonDataset(Dataset):
     def __getitem__(self, idx):
         """Get the idx sample of data"""
         return self.samples[idx]
+
+    def show(self):
+        plt.figure()
+        plt.plot(self.samples[:, 0], self.samples[:, 1], '.')
+        plt.title('MoonDataset for ' + str(len(self)) + ' samples with Gaussian noise std = ' + str(self.noise))
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.show()
+
