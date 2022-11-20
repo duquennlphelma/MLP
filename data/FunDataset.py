a54488d2083a8312f5fe54de51eef2e6527fd977
battoubattou <alicebatte@hotmail.fr>
2022-11-20 17:17:26 +0100

    download arg in fundataset

--- data/FunDataset.py
+++ data/FunDataset.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,29 +9,59 @@ class FunDataset(Dataset):
 
     """Characterize the fun dataset for pytorch """
 
-    def __init__(self, n_sample: int, noise=None, transform=None):
+    def __init__(self, n_sample: int, noise=None, transform=None, download=False):
         """Initialisation
         :param n_sample: Number of samples created.
         :param noise: Standard deviation of Gaussian noise added to the data.
         """
+        directory = '/Users/alicebatte/Documents/TUB/PML/MLP/sandbox'
+        file_name = 'fun_' + str(n_sample) + '_' + str(noise) + '_' + str(transform) + '.csv'
+        path = os.path.join(directory, file_name)
 
-        im = cv2.imread('shrek.jpg')
-        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        #cv2.imwrite('../sandbox/TUB_Logo_gray.jpg', im_gray)
-        th, im_th = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY)
-       # cv2.imwrite('../sandbox/TUB_Logo_bin.jpg', im_th)
+        if download:
+            if os.path.exists(path):
+                self.samples = np.loadtxt(path, dtype=float, delimiter=',')
+            else:
+                im = cv2.imread('shrek.jpg')
+                im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                # cv2.imwrite('../sandbox/TUB_Logo_gray.jpg', im_gray)
+                th, im_th = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY)
+                # cv2.imwrite('../sandbox/TUB_Logo_bin.jpg', im_th)
 
-        sequence = []
-        for i in range(len(im_th) - 1):
-            for j in range(len(im_th[0])):
-                if im_th[i, j]==0:
-                    sequence.append([i, j])
-        clean_samples = np.array(random.choices(sequence, weights=None, cum_weights=None, k=n_sample))
-        if noise==None:
-            self.samples=clean_samples
+                sequence = []
+                for i in range(len(im_th) - 1):
+                    for j in range(len(im_th[0])):
+                        if im_th[i, j] == 0:
+                            sequence.append([i, j])
+                clean_samples = np.array(random.choices(sequence, weights=None, cum_weights=None, k=n_sample))
+                if noise is None:
+                    self.samples = clean_samples
+                else:
+                    random_noise = np.random.normal(np.mean(clean_samples), noise,
+                                                    [len(clean_samples), len(clean_samples[0])])
+                    self.samples = clean_samples + random_noise
+                np.savetxt(path, self.samples, delimiter=',', fmt='%f')
+
         else:
-            random_noise = np.random.normal(np.mean(clean_samples), noise, [len(clean_samples), len(clean_samples[0])])
-            self.samples=clean_samples + random_noise
+            im = cv2.imread('shrek.jpg')
+            im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            # cv2.imwrite('../sandbox/TUB_Logo_gray.jpg', im_gray)
+            th, im_th = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY)
+            # cv2.imwrite('../sandbox/TUB_Logo_bin.jpg', im_th)
+
+            sequence = []
+            for i in range(len(im_th) - 1):
+                for j in range(len(im_th[0])):
+                    if im_th[i, j] == 0:
+                        sequence.append([i, j])
+            clean_samples = np.array(random.choices(sequence, weights=None, cum_weights=None, k=n_sample))
+            if noise is None:
+                self.samples = clean_samples
+            else:
+                random_noise = np.random.normal(np.mean(clean_samples), noise,
+                                                [len(clean_samples), len(clean_samples[0])])
+                self.samples = clean_samples + random_noise
+
         self.noise = noise
         self.transform = transform
 
@@ -50,7 +81,8 @@ class FunDataset(Dataset):
         plt.ylabel('y')
         plt.show()
 
-test=FunDataset(2000, 10, None)
+
+test=FunDataset(200, 10, None)
 test.show()
 
 
