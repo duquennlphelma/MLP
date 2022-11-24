139f7339736c4797ea9b4218605dede1c1ade75a
Tobiasz-Budzynski <BtobiasZ@protonmail.com>
2022-11-24 19:07:37 +0100

    loss function corrected
    Ongoing work, main file still needs work.

--- model/loss.py
+++ model/loss.py
@@ -1,9 +1,3 @@
-import numpy as np
-
-# todo: avoid numpy or change it to torch
-
-def loss(x_input, z_output, s_sum):
-    sum_of_probabilities_of_z = 0
-    for i in range(x_input.shape[0]):
-        sum_of_probabilities_of_z += 1 / 2 / np.pi * np.exp(-z_output[i, :].T @ z_output[i, :] / 2)
-    return - sum_of_probabilities_of_z - s_sum
+import torch
+def loss(z_output, s_sum):
+    return - torch.tensordot(z_output,z_output, dims=([-1], [-1])) - s_sum
