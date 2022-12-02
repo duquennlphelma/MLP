9d39085201cc2df619ce448c5787b92961039fbf
duquennlphelma <louise.duquenne@grenoble-inp.org>
2022-12-02 11:00:07 +0100

    shell_script

--- RNVP_shell_script.sh
+++ RNVP_shell_script.sh
@@ -6,5 +6,5 @@
 #$ -V              # provide environment variables
 #$ -M louisedqne@gmail.com
 
-#$ echo “Staring Real NVP script”
-#$ python3 train.py  # here you perform any commands to start your program
+echo “Staring Real NVP script”
+python3 train.py  # here you perform any commands to start your program
