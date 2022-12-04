6348dc7ae0b0654f1d14966d23573806d3e1c0eb
duquennlphelma <louise.duquenne@grenoble-inp.org>
2022-12-04 12:24:27 +0100

    _

--- model/rnvp/loss_function.py
+++ model/rnvp/loss_function.py
@@ -2,7 +2,8 @@ import numpy as np
 import torch
 
 def loss_log(z, det_J):
-    N=len(z)
+    z_detach= z.torch.tensor.detach()
+    N=len(z_detach)
     print(N)
-    pz= -N/2 *np.log(2*np.pi) -1/2 * torch.sum(torch.mul(z,z))
+    pz= -N/2 *np.log(2*np.pi) -1/2 * torch.sum(torch.mul(z_detach,z_detach))
     return (np.log(pz) + det_J)
