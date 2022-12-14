c91780928f0efbb597707dd80c2cf14d11382519
duquennlphelma <louise.duquenne@grenoble-inp.org>
2022-12-14 19:55:41 +0100

    _

--- model/rnvp/loss_function.py
+++ model/rnvp/loss_function.py
@@ -10,8 +10,8 @@ def loss_log(z, det_J):
 
     #multidimensional normal law
     pz = -N/2 * np.log(2*np.pi) - 1/2 * torch.sum(torch.mul(z_detach,z_detach))
-
-    print("len pz and pz \n", len(pz))
+    print("torch.mul chelou", torch.mul(z_detach,z_detach))
+    print("len pz and pz \n", pz)
     #are we really summing over the batch doing torch.sum(det_J) ? yes we are
     print("which size is det_J?\n", len(det_J))
     return pz - torch.sum(det_J)
