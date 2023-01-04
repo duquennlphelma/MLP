aa3593957e49d3b4a01ef3b1bc461b299de6131d
duquennlphelma <louise.duquenne@grenoble-inp.org>
2023-01-04 16:24:53 +0100

    _

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def load_data(dataset: str, transformation=None, n_train=None, n_test=None, nois
 def train_apply(model, dataset: str, epochs=10, batch_size=32, lr=0.01, momentum=0.0):
     _, train_loader, _, test_loader = load_data(dataset, transformation=None, n_train=100, n_test=100, noise=0.1,
                                                 batch_size=batch_size, shuffle=True, download=False)
-    optimizer = torch.optim.Adam([p for p in model.parameters() if p.requires_grad==True], lr=0.01)
+    optimizer = torch.optim.Adam([p for p in model.parameters() if p.requires_grad==True], lr=1e-4)
     #optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
     epoch_loss = []
