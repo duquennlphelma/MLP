6c7c533239eed6013ee11254da9e9a102844dbae
duquennlphelma <louise.duquenne@grenoble-inp.org>
2023-01-05 11:59:12 +0100

    _

--- model/rnvp/loss_function.py
+++ model/rnvp/loss_function.py
@@ -3,7 +3,12 @@ import torch
 
 
 def loss_log(z, det_J):
-
+    prior_ll = -0.5 * (z ** 2 + np.log(2 * np.pi))
+    prior_ll = prior_ll.view(z.size(0), -1).sum(-1) \
+               - np.log(2) * np.prod(z.size()[1:])
+    ll = prior_ll + det_J
+    nll = -ll.mean()
+    """
     # print('z:\n', z)
 
     N = len(z)
@@ -11,4 +16,7 @@ def loss_log(z, det_J):
     #multidimensional normal law
     pz = -N/2 * np.log(2*np.pi) - 1/2 * torch.sum(torch.mul(z, z))
 
-    return -pz + torch.sum(det_J)
+    return -pz + torch.sum(det_J) """
+
+    return nll
+
