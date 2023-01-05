d7bace013e11bd779b11f6248646913d5ecbebe3
battoubattou <alicebatte@hotmail.fr>
2023-01-05 16:38:02 +0100

    Comments and clean

--- train.py
+++ train.py
@@ -14,8 +14,20 @@ path_data_cluster = '/home/space/datasets/MNIST'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def load_data(dataset: str, transformation=None, n_train=None, n_test=None, noise=None, batch_size=32, shuffle=True, download= False):
-    """Loading of the dataset"""
+def load_data(dataset: str, transformation=None, n_train=None, n_test=None, noise=None, batch_size=32, shuffle=True,
+              download=False):
+    """
+    Loading of the dataset
+    :param dataset: Name of the datset (MoonDataset, FunDataset or MNIST)
+    :param transformation
+    :param n_train: number of training points generated
+    :param n_test: number of testing points generated
+    :param noise: noise added to the data
+    :param batch_size : size of the batches
+    :param shuffle: if True the data points are shuffled in the batches
+    :param download
+    :return Dataloader & Dataset
+    """
 
     # Default settings
     if not n_train:
@@ -25,23 +37,23 @@ def load_data(dataset: str, transformation=None, n_train=None, n_test=None, nois
 
     if dataset == 'FunDataset':
         directory = FunDataset.DIRECTORY
-        train_dataset = FunDataset.FunDataset(n_train, noise= noise, transform=transformation, download=download)
+        train_dataset = FunDataset.FunDataset(n_train, noise=noise, transform=transformation, download=download)
         train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-        test_dataset = FunDataset.FunDataset(n_test, noise= noise, transform=transformation,  download=download)
+        test_dataset = FunDataset.FunDataset(n_test, noise=noise, transform=transformation, download=download)
         test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
 
     elif dataset == 'MoonDataset':
         directory = MoonDataset.DIRECTORY
-        train_dataset = MoonDataset.MoonDataset(n_train, noise= noise, transform=transformation, download=download)
+        train_dataset = MoonDataset.MoonDataset(n_train, noise=noise, transform=transformation, download=download)
         train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-        test_dataset = MoonDataset.MoonDataset(n_test, noise= noise, transform=transformation,  download=download)
+        test_dataset = MoonDataset.MoonDataset(n_test, noise=noise, transform=transformation, download=download)
         test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
 
     elif dataset == 'MNIST':
         directory = path_data_cluster
-        train_dataset = torchvision.datasets.MNIST(directory, train=True, transform=transformation,  download=download)
+        train_dataset = torchvision.datasets.MNIST(directory, train=True, transform=transformation, download=download)
         train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-        test_dataset = torchvision.datasets.MNIST(directory, train=False, transform=transformation,  download=download)
+        test_dataset = torchvision.datasets.MNIST(directory, train=False, transform=transformation, download=download)
         test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
 
     else:
@@ -55,15 +67,26 @@ def load_data(dataset: str, transformation=None, n_train=None, n_test=None, nois
 
 
 def train_apply(model, dataset: str, epochs=10, batch_size=32, lr=1e-4, momentum=0.0):
+    """
+    Training the model
+    :param model: model chosen
+    :param dataset: dataset to train on
+    :param epochs: number of epochs to train on
+    :param batch_size: size of the batches to put the dat into
+    :param lr: learning rate
+    :param momentum:
+    :return: list of loss value per epoch
+    """
     _, train_loader, _, test_loader = load_data(dataset, transformation=None, n_train=1000, n_test=100, noise=0.1,
                                                 batch_size=batch_size, shuffle=True, download=False)
-    optimizer = torch.optim.Adam([p for p in model.parameters() if p.requires_grad==True], lr=lr)
-    #optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
+    optimizer = torch.optim.Adam([p for p in model.parameters() if p.requires_grad is True], lr=lr)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+    # Training metrics
     epoch_loss = []
 
-    # Train the model epochs * times
-    # Collect training metrics progress over the training
+    # Train the model epochs * times & Collect metrics progress over the training
     for i in range(epochs):
         epoch_loss_i = train_one_epoch(model, train_loader, optimizer)
         epoch_loss.append(epoch_loss_i)
@@ -74,37 +97,20 @@ def train_apply(model, dataset: str, epochs=10, batch_size=32, lr=1e-4, momentum
 
 
 if __name__ == "__main__":
-    #Dowload a MoonDataset example
-    #data_Moon, train_Moon, _, _ = load_data('MoonDataset', transformation=None, n_train=100, n_test=100, noise=0.1, download=False)
-
+    # Dowload a MoonDataset example
+    # data_Moon, train_Moon, _, _ = load_data('MoonDataset', transformation=None, n_train=100, n_test=100, noise=0.1,
+    #                                        download=False)
     # Dowload a FunDataset example
-    data_Fun, train_Fun, _, _ = load_data('FunDataset', transformation=None, n_train=1000, n_test=100, noise=0.1, download=False)
+    _, _, data_Fun, test_Fun = load_data('FunDataset', transformation=None, n_train=1000, n_test=1000, noise=0.1,
+                                         download=False)
 
-    #Download MNIST
-    # data_MNIST, train_MNIST, _, _ = load_data('MNIST', transformation=None, n_train=100, n_test=100, download=True)
+    # Plotting example of the data
+    data_Fun_array = [data_Fun[i] for i in range(len(data_Fun))]
+    show(data_Fun_array, 'plot_before_training_Fun_Dataset')
 
-    #Creating the model
+    # Creating the model
     model_rnvp = RNVP(2, 1)
-
-    # Passing MoonData into the model
-    exit_array = np.array([[0,0]])
-    for element in train_Fun:
-
-
-        exit_data = model_rnvp(element)
-
-        exit_data = exit_data[0].detach().numpy()
-
-        exit_array=np.concatenate((exit_array,exit_data))
-
-    # Plot the data
-    exit_array = np.array(exit_array[1:])
-    show(exit_array, 'plot_before_training_Fun_Dataset')
-    directory_fig = '/home/pml_07/MLP'
-
-
-
-
+    # Training
     out = train_apply(model_rnvp, 'FunDataset', 250, batch_size=25)
     directory = '/home/pml_07/MLP'
     file_name = 'epoch_loss' + '.png'
@@ -116,24 +122,20 @@ if __name__ == "__main__":
     print('Final output')
     print(out)
 
+    # Test
 
-
-
-    #Passing MoonData into the model
-    exit_array_bis = np.array([[0,0]])
-    for element in train_Fun:
+    # Passing MoonData into the model
+    exit_array_test = np.array([[0, 0]])
+    for element in test_Fun:
         exit_data = model_rnvp(element)
-
         exit_data = exit_data[0].detach().numpy()
-
-        exit_array_bis=np.concatenate((exit_array_bis,exit_data))
-
-
-
+        exit_array_test = np.concatenate((exit_array_test, exit_data))
 
     # Plot the data
+    exit_array_test = np.array(exit_array_test[1:])
 
-    exit_array_bis = np.array(exit_array_bis[1:])
+    print('EXIT ARRAY', exit_array_test)
+    show(exit_array_test, 'plot_after_training_Fun_Dataset')
 
     print('EXIT ARRAY', exit_array_bis)
     show(exit_array_bis, 'plot_after_training_Fun_Dataset')
@@ -153,16 +155,3 @@ if __name__ == "__main__":
     exit_array_bis = np.array(exit_data)
     print('EXIT ARRAY', exit_array_bis)
     show(exit_array_bis, 'plot_dataset_recreated.png')
-
-
-
-
-
-
-
-
-
-
-
-
-
