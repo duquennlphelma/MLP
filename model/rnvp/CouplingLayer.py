d1713a3c188e720e01b4a7205f3ece319c170416
duquennlphelma <louise.duquenne@grenoble-inp.org>
2023-01-17 18:28:01 +0100

    _

--- model/rnvp/CouplingLayer.py
+++ model/rnvp/CouplingLayer.py
@@ -37,7 +37,6 @@ class CouplingLayer(nn.Module):
         :return: output of the layer
         """
         # x = torch.Tensor(x)
-        print('x:\n', x)
         x=x[0]
         size = x.size()  # returns (batch_size, n_channels, h, w)
         b = utils.checkerboard_mask(size[-2], size[-1], reverse_mask=self.reverse)
