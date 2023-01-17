772d89ccdcfb0be002b0d78eef97f1db018dced1
duquennlphelma <louise.duquenne@grenoble-inp.org>
2023-01-17 17:43:57 +0100

    _

--- train.py
+++ train.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     for i, data in enumerate(test_MNIST):
         exit_data = model_rnvp(data[0])
         exit_data = exit_data[0].detach().numpy()
-        print('len(exit_data)',np.shape(exit_data))
+        print('len(exit_data)',np.shape(exit_data[0,0]))
         print('len(exit_array_test', np.shape(exit_array_test))
         exit_array_test = np.concatenate((exit_array_test, exit_data[0,0]))
 
