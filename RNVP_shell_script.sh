ea93c78e363a6f44ec2d6c5ef201b1f722c2f48b
battoubattou <alicebatte@hotmail.fr>
2023-01-18 17:28:29 +0100

    -

--- RNVP_shell_script.sh
+++ RNVP_shell_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#$ -N pml_O7_train_50_epochs  # name of the experiment: set consistent base name for output and error file (allows for easy deletion alias)
+#$ -N pml_07_MNIST_1000_epochs  # name of the experiment: set consistent base name for output and error file (allows for easy deletion alias)
 #$ -l cuda=1   # remove this line when no GPU is needed!
 #$ -q all.q       # don't fill the qlogin queue
 #$ -cwd          # start processes in current directory
 #$ -V              # provide environment variables
 #$ -M louisedqne@gmail.com
 
-echo “Staring Real NVP script”
+echo “Starting Real NVP script”
 python3 train.py  # here you perform any commands to start your program
