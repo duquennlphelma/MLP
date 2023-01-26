7a8d2e7f3969b5d9a8869ab27df7972b54cef0e2
battoubattou <alicebatte@hotmail.fr>
2023-01-26 11:51:10 +0100

    multi-scale architecture of the RNVP model

--- model/rnvp/RNVP.py
+++ model/rnvp/RNVP.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from model.rnvp.CouplingLayer import CouplingLayer
 import numpy as np
+import utils
 
 
 # todo generalize for n coupling layer in the network
@@ -16,30 +17,52 @@ class RNVP(nn.Module):
     def __init__(self, input_size, d):
         super().__init__()
 
-        layer_0 = CouplingLayer(input_size, d, reverse=True)
-        layer_1 = CouplingLayer(input_size, d, reverse=False)
-        layer_2 = CouplingLayer(input_size, d, reverse=True)
-        layer_3 = CouplingLayer(input_size, d, reverse=False)
-        layer_4 = CouplingLayer(input_size, d, reverse=True)
-        layer_5 = CouplingLayer(input_size, d, reverse=False)
+        layer_0 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=True)
+        layer_1 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=False)
+        layer_2 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=True)
+
+        # self.squeeze = nn.Conv2d(1, 4, kernel_size=3, padding=1)
+
+        layer_3 = CouplingLayer(4, 128, mask_type='channel_wise', reverse=False)
+        layer_4 = CouplingLayer(4, 128, mask_type='channel_wise', reverse=True)
+        layer_5 = CouplingLayer(4, 128, mask_type='channel_wise', reverse=False)
+
+        # self.de_squeeze1 = nn.Conv2d(4, 2, kernel_size=3, padding=1)
+
+        layer_6 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=True)
+        layer_7 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=False)
+        layer_8 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=True)
+        layer_9 = CouplingLayer(1, 64, mask_type='checkerboard', reverse=True)
+
+        # self.de_squeeze2 = nn.Conv2d(2, 1, kernel_size=3, padding=1)
 
         self.input_size = input_size
         self.d = d
-
-        self.layers = nn.ModuleList([layer_0, layer_1, layer_2, layer_3, layer_4, layer_5])
+        self.layers_check1 = nn.ModuleList([layer_0, layer_1, layer_2])
+        self.layers_channel = nn.ModuleList([layer_3, layer_4, layer_5])
+        self.layers_check2 = nn.ModuleList([layer_6, layer_7, layer_8, layer_9])
 
     def forward(self, x: torch.Tensor):
         """
         Forward pass of the RNVP network making the data pass into each coupling layers.
         """
-        x=x[0]
+        x = x[0]
         y = x
-        #sum_det_J is size batch_size,1
         sum_det_J = torch.zeros(len(x))
-        for i in range(len(self.layers)):
-            y, det_J = self.layers[i].forward(y)
+        for i in range(len(self.layers_check1)):
+            y, det_J = self.layers_check1[i].forward(y)
+            sum_det_J += det_J
 
-            # summing s_x from all the coupling layers : that's LDJ : log determinant jacobian
+        y = utils.squeeze(y)
+
+        for i in range(len(self.layers_channel)):
+            y, det_J = self.layers_channel[i].forward(y)
+            sum_det_J += det_J
+
+        y = utils.unsqueeze(y)
+
+        for i in range(len(self.layers_check2)):
+            y, det_J = self.layers_check2[i].forward(y)
             sum_det_J += det_J
 
         return y, sum_det_J
@@ -48,11 +71,23 @@ class RNVP(nn.Module):
         """
         Inverse pass of the RNVP network making the data go back through each coupling layers.
         """
-
         x = y
         sum_det_J = torch.zeros(len(y))
-        for i in range(1, len(self.layers)+1):
-            x, det_J = self.layers[len(self.layers)-i].inverse(x)
+
+        for i in range(1, len(self.layers_check2) + 1):
+            x, det_J = self.layers_check2[len(self.layers_check2) - i].inverse(x)
+            sum_det_J += det_J
+
+        x = utils.squeeze(x)
+
+        for i in range(1, len(self.layers_channel) + 1):
+            x, det_J = self.layers_channel[len(self.layers_channel) - i].inverse(x)
+            sum_det_J += det_J
+
+        x = utils.unsqueeze(x)
+
+        for i in range(1, len(self.layers_check1) + 1):
+            x, det_J = self.layers_check1[len(self.layers_check1) - i].inverse(x)
             sum_det_J += det_J
 
         return x, sum_det_J
