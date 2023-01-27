80442e7596648ce1014ead38801e0b8473a3093a
battoubattou <alicebatte@hotmail.fr>
2023-01-27 16:49:21 +0100

    cleaned RNVP2 code

--- model/rnvp/RNVP2.py
+++ model/rnvp/RNVP2.py
@@ -1,19 +1,18 @@
 import torch
 from torch import nn
 from model.rnvp.CouplingLayer2 import CouplingLayer
-import numpy as np
-
-
-# todo generalize for n coupling layer in the network
-# todo generalize for more than dimensions of points
 
 
 class RNVP(nn.Module):
     """
-    RNVP network model for 2 layers, using CouplingLayer2
+    RNVP network model adapted to 2D Dataset transformation using CouplingLayer2.
     """
 
-    def __init__(self, input_size, d):
+    def __init__(self, input_size=2, d=1):
+        """RNVP constructor for one scale : 3 checkerboard, 4 channel-wise and 4 checkerboard
+        :param input_size: size of an input sample (2 for Moon & Fun)
+        :param d: size of the modified part of the tensor into the CouplingLayer
+        """
         super().__init__()
 
         layer_0 = CouplingLayer(input_size, d, up=True)
@@ -32,15 +31,12 @@ class RNVP(nn.Module):
         """
         Forward pass of the RNVP network making the data pass into each coupling layers.
         """
-
         y = x
-        #sum_det_J is size batch_size,1
-        sum_det_J = torch.zeros(len(x))
+        sum_det_J = torch.zeros(len(x))  # sum_det_J is size (batch_size, 1)
+
         for i in range(len(self.layers)):
             y, det_J = self.layers[i].forward(y)
-
-            # summing s_x from all the coupling layers : that's LDJ : log determinant jacobian
-            sum_det_J += det_J
+            sum_det_J += det_J  # summing s_x from all the coupling layers : that's LDJ : log determinant Jacobian
 
         return y, sum_det_J
 
@@ -48,11 +44,11 @@ class RNVP(nn.Module):
         """
         Inverse pass of the RNVP network making the data go back through each coupling layers.
         """
-
         x = y
         sum_det_J = torch.zeros(len(y))
-        for i in range(1, len(self.layers)+1):
-            x, det_J = self.layers[len(self.layers)-i].inverse(x)
+
+        for i in range(1, len(self.layers) + 1):
+            x, det_J = self.layers[len(self.layers) - i].inverse(x)
             sum_det_J += det_J
 
         return x, sum_det_J
