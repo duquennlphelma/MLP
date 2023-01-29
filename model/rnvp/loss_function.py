cf1e5765125c0470df2018c25d86f64cb9ffdf97
duquennlphelma <louise.duquenne@grenoble-inp.org>
2023-01-29 14:59:01 +0100

    adaptation od the moss to work on any dimension

--- model/rnvp/loss_function.py
+++ model/rnvp/loss_function.py
@@ -12,7 +12,10 @@ class NLL(nn.Module):
         self.prior = torch.distributions.normal.Normal(loc=0.0, scale=1.0)
 
     def forward(self, z, det_J):
-        log_pz = self.prior.log_prob(z).sum(dim=[1, 2, 3])
+        if len(z.size())>1:
+            log_pz = self.prior.log_prob(z).sum(dim=[1, 2, 3])
+        else :
+            log_pz = self.prior.log_prob(z)
         log_px = det_J + log_pz
         nll = -log_px
         bpd = nll * np.log2(np.exp(1)) / np.prod(z.shape[1:])
